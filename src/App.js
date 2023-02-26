8b6bbf7f6859069e29402e05b98097debc46f332
Nithish651 <nithish651@gmail.com>
2023-02-26 11:33:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
+import { Power3, gsap } from "gsap/gsap-core";
+import { CSSPlugin } from "gsap/CSSPlugin";
+
+function App() {
+  gsap.registerPlugin(CSSPlugin);
+
+  const [showHome, setShowhome] = useState(false);
+  useEffect(() => {
+    let tl = gsap.timeline();
+    let duration = 3;
+    let bannerAnimation = {
+      duration: duration,
+      transform: "scale(1.5, 1.5)",
+      opacity: 0,
+      y: -50,
+      ease: Power3.easeOut,
+    };
+    tl.to(banner, bannerAnimation)
+      .call(removeElement(banner, duration * 1000))
+      .call(() => {
+        document.getElementById("root").style.overflow = "initial";
+        setShowhome(true);
+      });
+  }, []);
+
+  let banner = useRef(null);
+
+  const removeElement = (element, time) => {
+    console.log(element);
+    setTimeout(() => element.remove(), time);
+  };
+
+  return (
+    <>
+      <div
+        ref={(el) => {
+          banner = el;
+        }}
+        className="banner"
+      ></div>
+
+      {showHome && <div>Home Page</div>}
+    </>
+  );
+}
+
+export default App;
